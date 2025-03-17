aa3e87cd8323a1cb1008d41730f2968a0f06304c
Disservin <disservin.social@gmail.com>
2025-03-17 20:59:41 +0100

    proper output

--- src/main.rs
+++ src/main.rs
@@ -48,13 +48,49 @@ fn main() {
         24, 25, 26, 27,
         28, 29, 30, 31,
     ]);
-    let output_buckets = SfMaterialCount::default();
 
+    let output_buckets = SfMaterialCount::default();
     let num_inputs = <InputFeatures as inputs::SparseInputType>::num_inputs(&inputs);
     let max_active = <InputFeatures as inputs::SparseInputType>::max_active(&inputs);
     let num_buckets = <SfMaterialCount as outputs::OutputBuckets<_>>::BUCKETS;
 
     let (graph, output_node) = build_network(num_inputs, max_active, num_buckets);
+
+    let saved_format = vec![
+        SavedFormat::new("l0b", QuantTarget::I16(255), Layout::Normal),
+        SavedFormat::new("l0w", QuantTarget::I16(255), Layout::Normal),
+        SavedFormat::new("pst", QuantTarget::I32(255), Layout::Normal),
+        SavedFormat::new("l1b", QuantTarget::I32(64 * 255), Layout::Normal).add_transform(
+            |graph, mut weights| {
+                let fact = graph.get_weights("l1_factb").get_dense_vals().unwrap();
+                add_factoriser(&mut weights, &fact, L2 + 1);
+                weights
+            },
+        ),
+        SavedFormat::new(
+            "l1w",
+            QuantTarget::I8(64),
+            Layout::Transposed(Shape::new(NUM_BUCKETS * (L2 + 1), L1)),
+        )
+        .add_transform(|graph, mut weights| {
+            let fact = graph.get_weights("l1_factw").get_dense_vals().unwrap();
+            let fact = SavedFormat::transpose(Shape::new(L2 + 1, L1), &fact);
+            add_factoriser(&mut weights, &fact, (L2 + 1) * L1);
+            weights
+        }),
+        SavedFormat::new("l2b", QuantTarget::I32(64 * 127), Layout::Normal),
+        SavedFormat::new(
+            "l2w",
+            QuantTarget::I8(64),
+            Layout::Transposed(Shape::new(NUM_BUCKETS * L3, L2 * 2)),
+        ),
+        SavedFormat::new("l3b", QuantTarget::I32(16 * 600), Layout::Normal),
+        SavedFormat::new(
+            "l3w",
+            QuantTarget::I8(600 * 16 / 127),
+            Layout::Transposed(Shape::new(NUM_BUCKETS, L3)),
+        ),
+    ];
 
     let mut trainer = Trainer::<RangerOptimiser, _, _>::new(
         graph,
@@ -62,23 +98,11 @@ fn main() {
         RangerParams::default(),
         inputs,
         output_buckets,
-        vec![
-            SavedFormat::new("l0b", QuantTarget::I16(255), Layout::Normal),
-            SavedFormat::new("l0w", QuantTarget::I16(255), Layout::Normal),
-            //
-            SavedFormat::new("pst", QuantTarget::I32(255), Layout::Normal),
-            //
-            SavedFormat::new("l1b", QuantTarget::I32(64 * 127), Layout::Normal),
-            SavedFormat::new("l1w", QuantTarget::I8(64), Layout::Normal),
-            //
-            SavedFormat::new("l2b", QuantTarget::I32(64 * 127), Layout::Normal),
-            SavedFormat::new("l2w", QuantTarget::I8(64), Layout::Normal),
-            //
-            SavedFormat::new("l3b", QuantTarget::I32(16 * 600), Layout::Normal),
-            SavedFormat::new("l3w", QuantTarget::I8(600 * 16 / 127), Layout::Normal),
-        ],
+        saved_format,
         false,
     );
+
+    trainer.mark_weights_as_input_factorised(&["l0w", "pst"]);
 
     println!("Params: {}", trainer.optimiser().graph.get_num_params());
 
@@ -89,7 +113,7 @@ fn main() {
             batch_size: 16_384,
             batches_per_superbatch: 1024,
             start_superbatch: 1,
-            end_superbatch: 10,
+            end_superbatch: 1,
         },
         wdl_scheduler: wdl::ConstantWDL { value: 0.0 },
         lr_scheduler: lr::StepLR {
@@ -108,7 +132,7 @@ fn main() {
     };
 
     let data_loader = {
-        let file_path = "G:\\stockfish-data\\leela96-filt-v2.min.binpack";
+        let file_path = "/mnt/g/stockfish-data/leela96-filt-v2.min.binpack";
         let buffer_size_mb = 1024;
         let threads = 8;
         fn filter(entry: &TrainingDataEntry) -> bool {
@@ -178,4 +202,12 @@ fn build_network(num_inputs: usize, max_active: usize, num_buckets: usize) -> (G
     // graph, output node
     let output_node = out.node();
     (builder.build(ExecutionContext::default()), output_node)
+}
+
+const NUM_BUCKETS: usize = <SfMaterialCount as outputs::OutputBuckets<_>>::BUCKETS;
+
+fn add_factoriser(weights: &mut [f32], factoriser: &[f32], size: usize) {
+    for i in 0..NUM_BUCKETS * size {
+        weights[i] += factoriser[i % size]
+    }
 }
